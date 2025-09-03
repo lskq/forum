3117ed388b4e4cc84f322215f48cab62de34d3b6
lskq <lskq012025@gmail.com>
2025-09-03 12:43:00 +0200

    Implement reading of comments

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import { client } from "@/sanity/client";
 import Post from "../components/post"
 import PostButton from "../components/postButton"
 
-/* const POSTS_QUERY = `*[
+const POSTS_QUERY = `*[
   _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`; */
+]|order(publishedAt desc)[0...12]{_id, author, publishedAt, body}`;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function IndexPage() {
-  /* const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options); */
+  const postsData = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  console.log(postsData[0].body);
+
+  const postsMarkup = postsData.map(post => { return ( <Post key={post._id} author={post.author} publishedAt={new Date(post.publishedAt).toLocaleString()} body={post.body}/> ) });
 
   return (
     <>
@@ -23,7 +25,7 @@ export default async function IndexPage() {
         <span className="italic">It's a forum. What more do you want?</span>
       </header>
       <main className="px-2">
-        <Post poster="Bob" datetime="2025-09-03 10:26" message="This is a test." />
+        {postsMarkup}
         <PostButton />
       </main>
     </>
--- components/post.tsx
+++ components/post.tsx
@@ -1,9 +1,11 @@
-export default function({poster, datetime, message}: {poster: string, datetime: string, message: string}) {
+import { PortableText } from "next-sanity"
+
+export default function({author, publishedAt, body}: {author: string, publishedAt: string, body: any}) {
     return (
         <div className="border-1 rounded-md px-1 mb-4">
-            <h2 className="font-semibold">{poster}</h2>
-            <h3 className="italic text-sm">{datetime}</h3>
-            <p>{message}</p>
+            <h2 className="font-semibold">{author}</h2>
+            <h3 className="italic text-sm">{publishedAt}</h3>
+            <PortableText value={body} />
         </div>
     )
 }
