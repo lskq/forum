505282e46ea7cd59c5eeb3c41dddc07d0d16e7cd
lskq <lskq012025@gmail.com>
2025-09-05 11:09:13 +0200

    Double submission refresh delay

--- components/input.tsx
+++ components/input.tsx
@@ -13,13 +13,13 @@ export default function input() {
 
     const submit = (x: FormData) => {
         push(x);
-        setTimeout(() => router.refresh(), 1000);
+        setTimeout(() => router.refresh(), 2000);
     }
 
     return (
         <form action={submit} className="w-full md:w-5/6">
             <input className="hidden" name="author" type="text" value={name!} readOnly />
-            <textarea name="text" className="w-full focus:outline-0 border-1 rounded-md px-1 mb-1" placeholder="Write your post here."/><br/>
+            <textarea name="text" className="w-full focus:outline-0 border-1 rounded-md px-1 mb-1" placeholder="Write your post here." /><br />
             <button type="submit" className="h-2/3 bg-foreground text-background rounded-md p-1 hover:bg-gray-500 active:bg-amber-300 active:text-black mb-4 mr-4">
                 Post
             </button>
